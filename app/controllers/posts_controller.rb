acf9294e1010e7fdb4c6cbba472a87525da7d61c
Bryan MacDiarmid <brymac@gmail.com>
2015-08-08 20:10:49 -0400

    Posts changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,21 +3,38 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   def edit
+    @post = Post.find(params[:id])
+  end
+  def new
+    @post = Post.new
   end
   def show
     @post = Post.find(params[:id])
   end
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+ 
+    redirect_to posts_path
   end
   private
     def post_params
-      params.require(:post).permit(:title, :contents)
+      params.require(:post).permit(:title, :contents, :public_date)
     end
 end
