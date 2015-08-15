b81659010c04ae31efc48aea1e13090fcb8c51dd
Bryan MacDiarmid <brymac@gmail.com>
2015-08-15 13:39:12 -0400

    Added featured projects to homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc).limit(1)
+    @projects = Project.all.order(created_at: :desc).limit(3)
   end
 end
