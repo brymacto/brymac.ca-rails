521cf009f3aa36da3dabb5018a700474d225a667
Bryan MacDiarmid <brymac@gmail.com>
2015-08-15 15:20:12 -0400

    Skills bullets updated with half-value bullet icon

--- app/helpers/skills_helper.rb
+++ app/helpers/skills_helper.rb
@@ -1,14 +1,19 @@
 module SkillsHelper
-  def skills_bullets(num)
+  def skills_bullets(rating)
     str = ''
+    num = rating.to_i
+    decimal = true if rating != num
     blanks = 5 - num
+    blanks -= 1 if decimal
     str << '<span class="rating">'
 
-    num.times.each do |n|
+    num.times.each do
       str << '<i class="fa fa-circle"></i>'
     end
 
-    blanks.times.each do |n|
+    str << '<i class="fa fa-adjust fa-rotate-180"></i>' if decimal
+
+    blanks.times.each do
       str << '<i class="fa fa-circle-o"></i>'
     end
 
