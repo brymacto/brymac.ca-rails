4e22d48a42e09df23d2b158677da2533f8aa5d3f
Bryan MacDiarmid <brymac@gmail.com>
2015-08-19 15:59:40 -0400

    Fixed problem with view when more than one 'featured' project present

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,7 +13,7 @@ class StaticPagesController < ApplicationController
     featured_projects = Project.where("featured = ?", true).order(public_date: :desc).limit(3)
     other_projects = Project.where.not("featured = ?", true).order(public_date: :desc).limit(3)
     @projects = []
-    @projects << featured_projects if (featured_projects != nil)
+    @projects.concat(featured_projects) if (featured_projects != nil)
     extras_needed = 3 - (featured_projects.count || 0)
 
     extras_needed.times do |n|
